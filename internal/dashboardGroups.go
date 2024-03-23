ce158cdd2bb84fd4cedb5b8ff657d5356004a2bb
genekkion <genekkion@live.com>
2024-03-23 12:58:20 +0800

    stuff

---
+++ internal/dashboardGroups.go
@@ -0,0 +1,101 @@
+package internal
+
+import (
+	"github.com/gorilla/csrf"
+	"html/template"
+	"net/http"
+)
+
+/*
+type BotEntry struct {
+	Id       int
+	Name     string
+	Platform string
+	Status   string
+}
+
+var fakeBotData = []BotEntry{
+	{
+		Id:       0,
+		Name:     "Product Devvo",
+		Platform: "Telegram",
+		Status:   "Active",
+	},
+	{
+		Id:       1,
+		Name:     "Scrum Masters",
+		Platform: "Discord",
+		Status:   "Inactive",
+	},
+}
+
+// /bots
+func (server *Server) dashboardBots(writer http.ResponseWriter,
+	request *http.Request) {
+	dashboardHandler(writer, "/htmx/bots", "/bots")
+}
+*/
+
+// /htmx/groups for both tab panel and search functionality
+func (server *Server) htmxGroups(writer http.ResponseWriter,
+	request *http.Request) {
+	if request.Method == "GET" {
+		//htmxBotsPanel(writer, request)
+	} else if request.Method == "POST" {
+		//htmxBotsSearch(writer, request)
+	} else {
+		http.Error(writer, "Method not allowed.", http.StatusMethodNotAllowed)
+	}
+}
+
+// /htmx/bots "GET" for tab panel
+func htmxGroupsPanel(writer http.ResponseWriter,
+	request *http.Request) {
+	tmpl, err := template.ParseFiles("./templates/htmx/bots.html")
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	tmpl.Execute(writer, map[string]interface{}{
+		"csrfToken": csrf.Token(request),
+	})
+}
+
+/*
+// /htmx/bots "POST" for search engine
+func htmxBotsSearch(writer http.ResponseWriter,
+	request *http.Request) {
+	err := request.ParseForm()
+	if err != nil {
+		log.Println("bots.go - htmxBotsSearch(), parse form")
+		log.Println(err)
+		return
+	}
+	search := request.FormValue("search")
+	var botData []BotEntry
+	for _, bot := range fakeBotData {
+		if strings.Contains(strings.ToLower(bot.Name), search) {
+			botData = append(botData, bot)
+		}
+	}
+	tmpl, err := template.ParseFiles("./templates/htmx/botSearch.html")
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	tmpl.Execute(writer, map[string]interface{}{
+		"bots": botData,
+	})
+}
+
+// /htmx/bots/modal
+func (server *Server) htmxBotModal(writer http.ResponseWriter,
+	request *http.Request) {
+	tmpl, err := template.ParseFiles("./templates/htmx/botModal.html")
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	tmpl.Execute(writer, map[string]interface{}{})
+}
+*/
